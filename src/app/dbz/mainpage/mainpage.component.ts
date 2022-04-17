32a0c658cffe92fa1739a14b0a3e139c191038df
Gusabio19 <gusabioalmaximo19@gmail.com>
2022-04-17 12:52:53 -0500

    practica2

--- src/app/dbz/mainpage/mainpage.component.ts
+++ src/app/dbz/mainpage/mainpage.component.ts
@@ -1,15 +1,39 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component, OnInit } from '@angular/core';
-
+import { Personaje } from '../interfaces/dbz.interface';
+ 
 @Component({
   selector: 'app-mainpage',
-  templateUrl: './mainpage.component.html',
-  styleUrls: ['./mainpage.component.css']
+  templateUrl: './mainpage.component.html'
 })
-export class MainpageComponent implements OnInit {
+export class MainpageComponent   {
+personajes : Personaje[] = [
+  {
+    nombre: 'Goku',
+    poder: 1500
+   },
+   {
+    nombre: 'Vegeta',
+    poder: 1200
+   },
+   {
+    nombre: 'Broly',
+    poder: 1500
+   },
+   {
+    nombre: 'Gohan',
+    poder: 1300
+   }
+];
+nuevo: Personaje={
+  nombre:'Maestro Roshi',
+  poder:1000
+}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+agregarNuevoPersonaje(argumento: Personaje){
+  console.log("pelonchas-->"+argumento.nombre);
+  this.personajes.push(argumento);
+}
+ 
 
 }
